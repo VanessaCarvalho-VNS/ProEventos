24320eb4d46a94b94bcd2cbccb015b0d564fb033
VanessaCarvalho-VNS <vnscarvalh@gmail.com>
2023-02-12 19:39:38 -0300

    Seção 4: Angular

--- Back/src/ProEventos.API/Startup.cs
+++ Back/src/ProEventos.API/Startup.cs
@@ -34,6 +34,7 @@ namespace ProEventos.API
                 Context => Context.UseSqlite(Configuration.GetConnectionString("Default"))
             );
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProEventos.API", Version = "v1" });
@@ -56,6 +57,10 @@ namespace ProEventos.API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors(x => x.AllowAnyHeader()
+                              .AllowAnyMethod()
+                              .AllowAnyOrigin());
 
             app.UseEndpoints(endpoints =>
             {
---
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.scss']
+})
+export class EventosComponent {
+  public eventos: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getEventos();
+  }
+
+  public getEventos(): void {
+    this.http.get('https://localhost:5001/api/eventos').subscribe(
+      Response => this.eventos = Response,
+      error => console.log(error)
+    );
+  }
+
+}
+
